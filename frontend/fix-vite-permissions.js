a90c6c875a1f7c0dec84158e2a5b95c27dea4552
kevotushap <kevotushap@gmail.com>
2025-06-13 16:47:56 +0300

    Fix: remove node_modules from repo, reset lockfiles, add postinstall for Vite permissions

---
+++ frontend/fix-vite-permissions.js
@@ -0,0 +1,10 @@
+// This script tries to fix vite permission issues on Linux, but does nothing on Windows
+const { exec } = require('child_process');
+if (process.platform !== 'win32') {
+  exec('chmod +x node_modules/.bin/vite', (err) => {
+    if (err) {
+      // Not fatal, just log
+      console.error('Could not chmod vite:', err);
+    }
+  });
+}
